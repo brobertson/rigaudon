7541e1119dfc983e415b64e12ffb2553c60905fc
Bruce Robertson <brobertson@mta.ca>
2012-09-07 09:21:58 -0300

    error checking

--- Scripts/preprocess.sh
+++ Scripts/preprocess.sh
@@ -18,6 +18,8 @@ lept_out=$TMP_DIR/lept_${filename}
 pgm=$TMP_DIR/${filenamebase_noext}.pgm
 unpapered_pgm=$TMP_DIR/unpapered_${filenamebase_noext}.pgm
 unpapered_pbm=$TMP_DIR/unpapered_${filenamebase_noext}.pbm
+
+#debugging
 #echo "file in: $1"
 #echo "file out: $2"
 #echo "lept_out: $lept_out"
@@ -25,10 +27,31 @@ unpapered_pbm=$TMP_DIR/unpapered_${filenamebase_noext}.pbm
 #echo "unpapered_pgm: $unpapered_pgm"
 #echo "unpapered_pbm: $unpapered_pbm"
 
-$LEPT_DIR/prog/skewtest $1 $lept_out
-convert $lept_out $pgm
-unpaper --no-deskew --overwrite $pgm $unpapered_pgm
-convert $unpapered_pgm $unpapered_pbm
+#check error states
+EXPECTED_ARGS=2
+E_BADARGS=65
+
+#proper number of arguments?
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: `basename $0` [filein] [fileout]"
+  exit $E_BADARGS
+fi
+
+#does the input file exist?
+if [ ! -f $1 ]
+then
+    echo the file \"$1\" does not exist
+    exit
+fi
+
+#also check for output file directory?
+
+
+$LEPT_DIR/prog/skewtest $1 $lept_out &&
+convert $lept_out $pgm &&
+unpaper --no-deskew --overwrite $pgm $unpapered_pgm &&
+convert $unpapered_pgm $unpapered_pbm &&
 convert $unpapered_pbm $2
 
 
