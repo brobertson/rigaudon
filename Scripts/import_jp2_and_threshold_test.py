7d6a762ba490c531b67a24cbd6a6479cc85cf566
Bruce Robertson <brobertson@mta.ca>
2012-10-22 20:48:41 +0200

    toy with other segmentations, and add png support for this script

--- Scripts/import_jp2_and_threshold_test.py
+++ Scripts/import_jp2_and_threshold_test.py
@@ -6,6 +6,12 @@ from pylab import imshow, gray, show
 from gamera.plugins import numpy_io
 #from gamera.core import *
 from gamera.plugins.listutilities import median
+from gamera.toolkits.ocr.classes import Page
+
+class StepTwo(Page):
+	def page_to_lines(self):
+		self.ccs_lines = self.img.projection_cutting(Tx=38, Ty=2, noise=8)
+
 def my_filter(imageIn):
 	count = 0
 	image = imageIn
@@ -14,7 +20,7 @@ def my_filter(imageIn):
 	median_black_area = median([cc.black_area()[0] for cc in ccs])
 	#also check for height?
 	for cc in ccs:
-		if(cc.black_area()[0] > (median_black_area * 5)):
+		if(cc.black_area()[0] > (median_black_area * 10)):
 			cc.fill_white()
 			del cc
 			count = count + 1
@@ -36,6 +42,7 @@ been called.
 def filter_and_bbox(imageIn):
    from gamera.toolkits.ocr.classes import Page
    my_filter(imageIn)
+   #p = StepTwo(imageIn)
    p = Page(imageIn)
    p.segment()
    return show_lines(p)
@@ -44,35 +51,39 @@ def my_application():
    from gamera.toolkits.ocr.classes import Page
    # Make the image variable a global variable
    # IF YOU DON'T DO THIS, THE WINDOW WILL DISAPPEAR!
-   global image, ots, ots_up, ots_mid, box_image
+   global image, ots, ots_up, ots_mid, box_image, ots_low
+   print sys.argv[-1]
+   #image_color = load_image(sys.argv[-1])
    picture = mh.imread(sys.argv[-1], as_grey=True)
    picture = picture.astype(np.uint8)
    # Load the image
-   image = numpy_io.from_numpy(picture)#load_image(sys.argv[-1])
-	
+   image = numpy_io.from_numpy(picture)#load_image(sys.argv[-1]
+   #image = image_color.to_greyscale()
    # Display the image in a window
    #image.simple_sharpen()
    ots = image.otsu_threshold()
    thresh = image.otsu_find_threshold()
+   thresh_low = int(thresh* 0.90)
    thresh_mid = int(thresh * 1.05)
-   thresh_plus = int(thresh * 1.15)
+   thresh_plus = int(thresh * 1.18)
    ots_up = image.threshold(thresh_plus)
    ots_mid = image.threshold(thresh_mid)
-   
+   ots_low = image.threshold(thresh_low)
    print "OTSU: " + str(thresh)
    print "OTSU Mid: " + str(thresh_mid)
    print "OTSU * 1.15: " + str(thresh_plus)
-   
+   ots_low = filter_and_bbox(ots_low)
    ots = filter_and_bbox(ots)
    ots_up = filter_and_bbox(ots_up)
    ots_up.display("OTSU+")
    #ots_mid = show_lines(p)
    #box_image.display()
    #my_filter(ots_mid)
-   #ots_up.display("OTSU+FIL")
+   ots_up.display("OTSU+FIL")
    ots_mid = filter_and_bbox(ots_mid)
    ots_mid.display("MID")
    ots.display("OTSU")
+   ots_low.display("OTSU_LOW")
    image.display()
 # Import the Gamera core and initialize it
 from gamera.core import *
