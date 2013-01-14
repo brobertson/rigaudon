52783512f9faaa21e7aaf09c77e2387ffd071a95
Bruce Robertson <brobertson@mta.ca>
2013-01-14 23:37:34 +0100

    add script to compare hocr files based on line and word location. This example uses greek_tools.py to test for 'greekness, and create a new file from abbyy in and gamera in.

--- Scripts/greek_tools.py
+++ Scripts/greek_tools.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python
+# -*- coding: utf8 -*- 
 import unicodedata
+
+def memoize(f):
+    cache= {}
+    def memf(*x):
+        if x not in cache:
+            cache[x] = f(*x)
+        return cache[x]
+    return memf
+
+
 def dump(stringIn):
         for char in stringIn:
                 try:
                         print '\t\t' + unicodedata.name(char) +" (" + unicodedata.category(char) + ")"
                 except ValueError:
                         print '\t\t' + char.encode('unicode-escape')
-
+@memoize
 def is_greek_char(char):
                 import re
                 return bool(re.match(ur'([\u0373-\u03FF]|[\u1F00-\u1FFF]|\u0300|\u0301|\u0313|\u0314|\u0345)',char,re.UNICODE))
 
-
-def is_greek_string(string):
+@memoize
+def is_greek_string(string_in):
+	import string,re
+	regex = re.compile('[%s]' % re.escape(string.punctuation + u'—'))
         greekness_threshold = 0.7
-        #print "String to test for greekness: ", string.encode('utf-8')
+        print "String to test for greekness: ", string_in.encode('utf-8')
+        string_in = regex.sub('',string_in)
+	print "cleaned string to test: ", string_in.encode('utf-8')
         count = 0
-        if string == None:
+        if string_in == None or len(string_in) < 1:
                 return False
-        for char in string:
+        for char in string_in:
                 if is_greek_char(char):
                         count = count + 1
-        factor = float(count) / float(len(string))
+        factor = float(count) / float(len(string_in))
         #print "factor: ", factor
         if factor > greekness_threshold:
                 return True
@@ -29,7 +44,7 @@ def is_greek_string(string):
                 return False
 
 
-
+@memoize
 def delete_non_greek_tokens(tokens):
         tokens_out = []
         for token in tokens:
--- Scripts/munge_hocr.py
+++ Scripts/munge_hocr.py
@@ -32,10 +32,10 @@ def rect_offset(rect1, rect2):
     return (offset_x,offset_y)
 
 
-def get_hocr_lines_for_file(fileIn):
-    tree = etree.parse(fileIn)
-    root = tree.getroot()
-    hocr_line_elements = tree.xpath("//html:span[@class='ocr_line'] | //span[@class='ocr_line']",namespaces={'html':"http://www.w3.org/1999/xhtml"})
+def get_hocr_lines_for_tree(treeIn):
+   
+    root = treeIn.getroot()
+    hocr_line_elements = treeIn.xpath("//html:span[@class='ocr_line'] | //span[@class='ocr_line']",namespaces={'html':"http://www.w3.org/1999/xhtml"})
     line_counter = 0
     lines_out = []
     for hocr_line_element in hocr_line_elements:
@@ -60,96 +60,159 @@ def get_hocr_lines_for_file(fileIn):
 
 def dump_lines(lines_out):
     for line_out in lines_out:
-        print 'LINE', line_out.element
-        for word_out in line_out.words:
-            print '\t', word_out.text, word_out.bbox,  word_out.element
+        print 'LINE', line_out.bbox 
+        #for word_out in line_out.words:
+        #    print '\t', word_out.text, word_out.bbox,  word_out.element
+
+def sort_lines_top_bottom_left_right(lines):
+    lines.sort(key=lambda line: line.bbox.lr_y)
+    lines.sort(key=lambda line: line.bbox.ul_x)
+    return lines
+
+def dump_words(words):
+    for word in words:
+        print word.text, word.bbox
+
+def dump_words_alone(words):
+    for word in words:
+        print word.text,
+    print
+    
+def total_x_offset(rect1, rect2):
+    print "doing x offset"
+    print rect1, rect2
+    difference = abs(rect1.ul_x - rect2.ul_x) + abs(rect1.lr_x - rect2.lr_x)
+    print difference
+    return difference
+
+def distance_between_uls(bbox1, bbox2):
+    import math
+    return math.sqrt((bbox1.ul_x - bbox2.ul_x)**2 + (bbox1.ul_y - bbox2.ul_y)**2)
 
 def compare_hocr_lines(lines1, lines2, x_tolerance, y_tolerance):
+    lines1 = sort_lines_top_bottom_left_right(lines1)
+    lines2 = sort_lines_top_bottom_left_right(lines2)
+    lines2_copy = lines2[:]
+    print "at start lines2 is: ", len(lines2)
+    line_pairs = []
     if not len(lines1) == len(lines2):
         raise ValueError("hocr lines are not equal in number")
-    pairs = zip(lines1, lines2)
-    for pair in pairs:
-        (offset_x, offset_y) = rect_offset(pair[0].bbox, pair[1].bbox)
-        if offset_y > y_tolerance:
-            raise ValueError('hocr lines do not align vertically within tolerance %s'%y_tolerance)
-        else:
-            print "hocr files pass alignment of ", len(lines1), " lines with tolerance of ", y_tolerance
-   # print "comparing words"
-    for pair in pairs:
+    for line1 in lines1:
+        #print "doing line1 ", line1.bbox
+        already_matched = False
+        min_distance_candidate = lines2_copy[0]
+        #print "initial mdc: ", min_distance_candidate.bbox
+        for line2 in lines2_copy[1:]:
+            #print "\t cp. line2: ", line2.bbox
+            l2d = distance_between_uls(line1.bbox, line2.bbox)#line1.bbox.distance_bb(line2.bbox)
+            mdcd = distance_between_uls(line1.bbox, min_distance_candidate.bbox)#line1.bbox.distance_bb(min_distance_candidate.bbox)
+            #print "\t mdcd: ", mdcd
+            #print "\t distance: ", l2d
+            #if distance_between_uls(line1.bbox, line2.bbox) < distance_between_uls(line1.bbox, min_distance_candidate.bbox):#
+            if l2d < mdcd:
+                #print "\t\tmade line2 mdc"
+                min_distance_candidate = line2
+        line_pairs.append((line1,min_distance_candidate))
+        lines2_copy.remove(min_distance_candidate)
+    if not len(line_pairs) == len(lines1):
+        raise ValueError("not all lines were matched")
+    print "at end lines2 is ", len(lines2)
+    max_x_offset = 0
+    max_y_offset = 0
+    for line_pair in line_pairs:
         print "new line"
-        words_1 = pair[0].words
-        words_2 = pair[1].words
+        words_1 = line_pair[0].words
+        words_2 = line_pair[1].words
         line_matches = []
         word_1_matches = []
         word_2_matches = []
-        for word in words_1:
-           # print
-         #   print '\t', word.bbox, word.text
-            for word_2 in words_2:
-                (offset_x, offset_y) = rect_offset(word.bbox, word_2.bbox)
-                if offset_x < x_tolerance:
-                    word_1_matches.append(word)
-                    word_2_matches.append(word_2)
-                    line_matches.append(([word],[word_2]))
-                   # print "\tI think is equal to:"
-                   # print '\t', word_2.bbox, word_2.text
-        #we've done the simple ones, now for the hard ones
-        unmatched_words_1 = [x for x in words_1 if x not in word_1_matches]
-        unmatched_words_2 = [x for x in words_2 if x not in word_2_matches]
-        current_word_1 = 0
+        unmatched_words_1 = line_pair[0].words
+        unmatched_words_2 = line_pair[1].words
+        current_word_1 = -1
         current_word_2 = 0
         left_match = []
         right_match = []
-
+        
         while current_word_1 < len(unmatched_words_1):
-            while current_word_2 < len(unmatched_words_2):
+            current_word_1 += 1
+            while current_word_2 < len(unmatched_words_2) and current_word_1 < len(unmatched_words_1):
                 left_difference = unmatched_words_1[current_word_1].bbox.ul_x - unmatched_words_2[current_word_2].bbox.ul_x
-                print "left difference: ", left_difference
+                #print "cw1: ", current_word_1
+                #print "cw2: ", current_word_2
+                #print "left difference: ", left_difference
+                right_difference = unmatched_words_1[current_word_1].bbox.lr_x - unmatched_words_2[current_word_2].bbox.lr_x
+                        
                 if abs(left_difference) < x_tolerance:
                     left_match.append(unmatched_words_1[current_word_1])
                     right_match.append(unmatched_words_2[current_word_2])
-                    right_difference = unmatched_words_1[current_word_1].bbox.lr_x - unmatched_words_2[current_word_2].bbox.lr_x
-                    print "right difference: ", right_difference
-                    while abs(right_difference) > x_tolerance and current_word_2 < len(unmatched_words_2)-1 and current_word_1 < len(unmatched_words_1)-1:
-                        print "did loop"
+                    while abs(right_difference) > x_tolerance and (current_word_2 < len(unmatched_words_2)-1 or current_word_1 < len(unmatched_words_1)-1):
+                        #print "did loop"
                         if right_difference > 0 and ((current_word_2 < len(unmatched_words_2)) or (unmatched_words_2[current_word_2 + 1].bbox.ul_x < unmatched_words_1[current_word_1].bbox.lr.x)):
                             #ensure that the next word of the second document is within the box of the first
+                            #print "adding one from right"
                             current_word_2 += 1
                             right_match.append(unmatched_words_2[current_word_2])
-                        elif right_difference < 0 and unmatched_words_1[current_word_1 + 1].bbox.ul_x < unmatched_words_2[current_word_2].bbox.lr.x:
+                        elif right_difference < 0 and ((current_word_1 < len(unmatched_words_1) or unmatched_words_1[current_word_1 + 1].bbox.ul_x < unmatched_words_2[current_word_2].bbox.lr.x)):
                             #ensured that the next word from the first document is within the box of the second
+                            #print"adding one from left"
                             current_word_1 += 1
                             left_match.append(unmatched_words_1[current_word_1])
                         else:#means that one of the above are bigger than they ought to be, so we'll just append what we have
+                            #print "breaking"
                             break
                         right_difference = unmatched_words_1[current_word_1].bbox.lr_x - unmatched_words_2[current_word_2].bbox.lr_x
-                    line_matches.append((left_match,right_match))     
+                        #print "right difference: ", right_difference
+                    #print "appending values"   
+                    line_matches.append((left_match,right_match))
+                    left_match=[]
+                    right_match= []
                 current_word_2 += 1
-            current_word_1 += 1
+            current_word_2 = 0
         for match in line_matches:
             (left_match, right_match) = match
             for word in left_match:
-                print word.text,
+                print word.text, word.element,
             print "\t\t\t\t",
             for word in right_match:
-                print  word.text, " ",
+                print  word.text, word.element
             print
-    return True
+            line_pair[1].line_matches = line_matches
+    print "lines2 on return: ", len(lines2)
+    return lines2
 
-
+def grecify_left(right_lines):
+    import unicodedata
+    print "doing grecify"
+    print 'linematches length: ', len(right_lines)
+    from greek_tools import is_greek_string
+    
+    for lines in right_lines:
+        for match in lines.line_matches:
+            (left_match, right_match) = match
+            test_word = ""
+            for word in right_match:
+               test_word += word.text
+            print "test_word: ", test_word
+            if is_greek_string(test_word):
+                print "replacing left"
+                left_match[0].element.text = unicodedata.normalize('NFD',test_word)
+		left_match[0].element.set("lang","grc")
+		left_match[0].element.set("{http://www.w3.org/XML/1998/namespace}lang","grc")
+    
 
 fileIn1 = open(sys.argv[1],'r')
+tree1 = etree.parse(fileIn1)
 fileIn2 = open(sys.argv[2],'r')
-lines_out_1 = get_hocr_lines_for_file(fileIn1)
-lines_out_2 = get_hocr_lines_for_file(fileIn2)
-#dump_lines(lines_out_1)
-#print "new"
-#dump_lines(lines_out_2)
+tree2 = etree.parse(fileIn2)
+lines_1 = get_hocr_lines_for_tree(tree1)
+lines_2 = get_hocr_lines_for_tree(tree2)
 
-#print "comparing..."
-#print "1 has : ",len(lines_out_1), "lines"
-#print "2 has : ",len(lines_out_2), "lines"
+my_line_matches = compare_hocr_lines(lines_1, lines_2,15,15)
+
+grecify_left(my_line_matches)
+
+out_file = open(sys.argv[3],'w')
+out_file.write(etree.tostring(tree1.getroot(), xml_declaration=True))
+out_file.close()
 
 
-
-compare_hocr_lines(lines_out_1, lines_out_2,2,2)
