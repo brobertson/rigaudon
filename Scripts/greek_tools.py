029b3541868e84fd5d8ad0085c7e91385af14c4f
Bruce Robertson <brobertson@mta.ca>
2013-01-30 08:52:08 -0500

    add split_text_token to greek_tools

--- Scripts/greek_tools.py
+++ Scripts/greek_tools.py
@@ -23,13 +23,34 @@ def is_greek_char(char):
                 return bool(re.match(ur'([\u0373-\u03FF]|[\u1F00-\u1FFF]|\u0300|\u0301|\u0313|\u0314|\u0345)',char,re.UNICODE))
 
 @memoize
+def is_greek_capital(char):
+	import re
+	return bool(re.match(ur'([\u0391-\u03A9])',char,re.UNICODE))
+
+@memoize
+def is_uc_word(string_in):
+	count = 0
+	threshold = 0.7
+        count = 0
+        if string_in == None or len(string_in) < 1:
+                return False
+        for char in string_in:
+                if is_greek_capital(char):
+                        count = count + 1
+        factor = float(count) / float(len(string_in))
+        #print "factor: ", factor
+        if factor > threshold:
+                return True
+        else:
+                return False
+@memoize
 def is_greek_string(string_in):
 	import string,re
 	regex = re.compile('[%s]' % re.escape(string.punctuation + u'—·' + u'0123456789'))
         greekness_threshold = 0.7
-        print "String to test for greekness: ", string_in.encode('utf-8')
-        string_in = regex.sub('',string_in)
-	print "cleaned string to test: ", string_in.encode('utf-8')
+        #print "String to test for greekness: ", string_in.encode('utf-8')
+        string_in = regex.sub('',string_in,re.UNICODE)
+	#print "cleaned string to test: ", string_in.encode('utf-8')
         count = 0
         if string_in == None or len(string_in) < 1:
                 return False
@@ -44,7 +65,7 @@ def is_greek_string(string_in):
                 return False
 
 
-@memoize
+#@memoize
 def delete_non_greek_tokens(tokens):
         tokens_out = []
         for token in tokens:
@@ -55,3 +76,11 @@ def delete_non_greek_tokens(tokens):
                         #print token.encode('utf-8'), " LATIN"
         return tokens_out
 
+def split_text_token(stringIn):
+	import re
+	word_parts = re.match(ur'(^["]*)([^.,!?;†·:〉\]]+)([.,!?;†·:〉\]]*$)',stringIn,re.UNICODE)
+   	try:
+      		parts = word_parts.groups()
+	except AttributeError:
+		parts = ('',stringIn,'')
+	return parts
