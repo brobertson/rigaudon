193d7a494881439d156e84ab5713dac81b1799fc
Bruce Robertson <brobertson@mta.ca>
2013-01-31 07:43:00 -0500

    turn off underdot- and macron- finding by default

--- Gamera/greekocr-1.0.0/gamera/toolkits/greekocr/singlediacritics.py
+++ Gamera/greekocr-1.0.0/gamera/toolkits/greekocr/singlediacritics.py
@@ -158,7 +158,7 @@ class SingleTextline(Textline):
             if g_name in ['greek.capital.lunate.sigma.symbol','greek.lunate.sigma.symbol']:
                self.check_capital_letter(g,height_limit_for_lc_omicron,'greek.lunate.sigma.symbol','greek.capital.lunate.sigma.symbol')
 
-   def identify_ambiguous_glyphs(self, delete_macron_below=False, check_for_underdots=True):
+   def identify_ambiguous_glyphs(self, delete_macron_below=False, check_for_underdots=False):
       if delete_macron_below:
          glyphs_copy = self.glyphs[:]
          i = 0
@@ -190,11 +190,11 @@ class SingleTextline(Textline):
             if g.center.y > self.bbox.center.y:
                g.classify_automatic("full.stop")
                if check_for_underdots:
-                  print "checking for underdots on something with x: ", g.center.x, " and Y: ", g.center.y
+                  #print "checking for underdots on something with x: ", g.center.x, " and Y: ", g.center.y
                   for other in self.glyphs:
-                     print "\tother is: ", other.get_main_id(), "with center_x ", other.center.x, " center_y: ", other.center.y
+                     #print "\tother is: ", other.get_main_id(), "with center_x ", other.center.x, " center_y: ", other.center.y
                      if (g.center.x > other.ul_x and g.center.x < other.lr_x) and (g.center.y > other.center.y) and "letter" in other.get_main_id():
-                        print "classifying as dot below"
+                        #print "classifying as dot below"
                         g.classify_automatic("combining.dot.below")
                         break
             else:
