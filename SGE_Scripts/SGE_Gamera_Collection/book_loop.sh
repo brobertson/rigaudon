9071e7758af0f3ac5e47ab78b3cf81470c0af5fb
Bruce Robertson <brobertson@mta.ca>
2013-02-03 20:48:06 -0500

    add spellcheck and Greek/Latin combining

--- SGE_Scripts/SGE_Gamera_Collection/book_loop.sh
+++ SGE_Scripts/SGE_Gamera_Collection/book_loop.sh
@@ -8,9 +8,13 @@ export PRIMARY_OUTPUT=$BOOK_DIR/${DATE}_${filename}_txt_output
 export SECONDARY_OUTPUT=$BOOK_DIR/${DATE}_${filename}_output_tc
 export TESS_OUTPUT=$BOOK_DIR/tess_eng_output
 export HOCR_SELECTED=$BOOK_DIR/${DATE}_${filename}_selected_hocr_output
+export SPELLCHECKED_HOCR_SELECTED=$BOOK_DIR/${DATE}_${filename}_selected_hocr_output_spellchecked
+export COMBINED_HOCR=$BOOK_DIR/${DATE}_${filename}_combined_hocr_output
 export TEXT_SELECTED=$BOOK_DIR/${DATE}_${filename}_selected_text_output
 export ABBYY_DATA=`dirname $BOOK_DIR`/${barebookname}_hocr
 export RELATIVE_HOCR_SELECTED=${DATE}_${filename}_selected_hocr_output
+export RELATIVE_SPELLCHECKED_HOCR_SELECTED=${DATE}_${filename}_selected_hocr_output_spellchecked
+export RELATIVE_COMBINED_HOCR=${DATE}_${filename}_combined_hocr_output
 export RELATIVE_TEXT_SELECTED=${DATE}_${filename}_selected_text_output
 export RELATIVE_HOCR_OUTPUT=${DATE}_${filename}_hocr_output
 export RELATIVE_PRIMARY_OUTPUT=${DATE}_${filename}_txt_output
@@ -18,11 +22,15 @@ export RELATIVE_SECONDARY_OUTPUT=${DATE}_${filename}_output_tc
 export RELATIVE_TESS_OUTPUT=tess_eng_output
 export CSV_FILE=$SECONDARY_OUTPUT/${DATE}_${filename}_summary.csv
 export GRAPH_IMAGE_FILE=$HOCR_SELECTED/${DATE}_${filename}_summary.png
+export DICTIONARY_FILE=/usr/local/OCR_Processing/Dictionary/Dicts_From_Perseus/all_perseus_dict2.csv
+export SPELLCHECK_FILE=$TEXT_SELECTED/${DATE}_${filename}_spellcheck.csv
 #touch $BOOK_DIR/filelist.txt
 rm -rf $PRIMARY_OUTPUT > /dev/null
 rm -rf $SECONDARY_OUTPUT > /dev/null 
 rm -rf $HOCR_OUTPUT > /dev/null
 rm -rf $HOCR_SELECTED > /dev/null
+rm -rf $SPELLCHECKED_HOCR_SELECTED > /dev/null
+rm -rf $COMBINED_HOCR > /dev/null
 rm -rf $TEXT_SELECTED > /dev/null
 rm -rf $TESS_OUTPUT > /dev/null
 mkdir $HOCR_OUTPUT
@@ -30,6 +38,8 @@ mkdir  $PRIMARY_OUTPUT
 mkdir  $SECONDARY_OUTPUT
 mkdir $TESS_OUTPUT > /dev/null
 mkdir $HOCR_SELECTED
+mkdir $SPELLCHECKED_HOCR_SELECTED
+mkdir $COMBINED_HOCR
 mkdir $TEXT_SELECTED
 
 #Job names
@@ -37,6 +47,9 @@ JOB_NAME_BASE=$barebookname-$DATE
 OCR_BATCH_JOB_NAME=$JOB_NAME_BASE-ocr-batch
 LYNX_DUMP_JOB_NAME=$JOB_NAME_BASE-lynx-dump
 SUMMARY_SPLIT_JOB_NAME=$JOB_NAME_BASE-summary-split
+SPELLCHECK_JOB_NAME=$JOB_NAME_BASE-spellcheck
+SPELLREPLACE_JOB_NAME=$JOB_NAME_BASE-spellreplace
+COMBINE_GREEK_AND_LATIN_JOB_NAME=$JOB_NAME_BASE-combine-hocrs
 POSTPROCESS_JOB_NAME=$JOB_NAME_BASE-postprocess
 echo "first previous: $PREVIOUS_BOOK_NAME"
 #Previous job name
@@ -68,4 +81,10 @@ qsub -N $LYNX_DUMP_JOB_NAME -p -150 -hold_jid $OCR_BATCH_JOB_NAME -o $OUTPUT_DIR
 #3. Make a graph of page# vs. score for these highest-scoring pages.
 qsub -N $SUMMARY_SPLIT_JOB_NAME -p -100 -hold_jid  $LYNX_DUMP_JOB_NAME  -b y -o $OUTPUT_DIR -e $ERROR_DIR -S /bin/bash -V /usr/bin/python $RIGAUDON_HOME/Scripts/summary_split.py $CSV_FILE $HOCR_OUTPUT $SECONDARY_OUTPUT $HOCR_SELECTED $TEXT_SELECTED $GRAPH_IMAGE_FILE
 
-qsub -N $POSTPROCESS_JOB_NAME -p 0 -hold_jid  $SUMMARY_SPLIT_JOB_NAME  -o $OUTPUT_DIR -e $ERROR_DIR -S /bin/bash -V $RIGAUDON_HOME/Scripts/post_qsub_processing.sh
+qsub -N $SPELLCHECK_JOB_NAME -pe make 20 -hold_jid  $SUMMARY_SPLIT_JOB_NAME -b y -o $OUTPUT_DIR -e $ERROR_DIR -S /bin/bash -V /usr/bin/python $RIGAUDON_HOME/Scripts/read_dict5.py $DICTIONARY_FILE $TEXT_SELECTED/output*  $SPELLCHECK_FILE
+
+qsub -N $SPELLREPLACE_JOB_NAME -hold_jid  $SPELLCHECK_JOB_NAME -b y -o $OUTPUT_DIR -e $ERROR_DIR -S /bin/bash -V /usr/bin/python $RIGAUDON_HOME/Scripts/spellcheck_hocr.py $SPELLCHECK_FILE $HOCR_SELECTED   $SPELLCHECKED_HOCR_SELECTED
+
+qsub -N $COMBINE_GREEK_AND_LATIN_JOB_NAME -hold_jid $SPELLREPLACE_JOB_NAME -b y -o $OUTPUT_DIR -e $ERROR_DIR -S /bin/bash -V  $RIGAUDON_HOME/Scripts/mungomatic.sh $ABBYY_DATA $SPELLCHECKED_HOCR_SELECTED $COMBINED_HOCR
+
+qsub -N $POSTPROCESS_JOB_NAME -p 0 -hold_jid  $COMBINE_GREEK_AND_LATIN_JOB_NAME  -o $OUTPUT_DIR -e $ERROR_DIR -S /bin/bash -V $RIGAUDON_HOME/Scripts/post_qsub_processing.sh
