f90b79d63908cf4108065a608116fa65e2510817
Bruce Robertson <brobertson@mta.ca>
2013-02-11 13:22:04 -0500

    process all the way to side-by-side view

--- SGE_Scripts/SGE_Gamera_Collection/book_loop.sh
+++ SGE_Scripts/SGE_Gamera_Collection/book_loop.sh
@@ -3,6 +3,8 @@
 filename=$(basename $CLASSIFIER_FILE)
 export filename=${filename%.*}
 export barebookname=${BOOK_NAME%%_jp2}
+export JPG_GRAY_IMAGES=$BOOK_DIR/${barebookname}_gray
+export JPG_COLOR_IMAGES=$BOOK_DIR/${barebookname}_color
 export HOCR_OUTPUT=$BOOK_DIR/${DATE}_${filename}_hocr_output
 export PRIMARY_OUTPUT=$BOOK_DIR/${DATE}_${filename}_txt_output
 export SECONDARY_OUTPUT=$BOOK_DIR/${DATE}_${filename}_output_tc
@@ -24,6 +26,7 @@ export CSV_FILE=$SECONDARY_OUTPUT/${DATE}_${filename}_summary.csv
 export GRAPH_IMAGE_FILE=$HOCR_SELECTED/${DATE}_${filename}_summary.png
 export DICTIONARY_FILE=/usr/local/OCR_Processing/Dictionary/Dicts_From_Perseus/all_perseus_dict2.csv
 export SPELLCHECK_FILE=$TEXT_SELECTED/${DATE}_${filename}_spellcheck.csv
+export SIDE_BY_SIDE_VIEW=$BOOK_DIR/${barebookname}_${DATE}_${filename}_sidebyside
 #touch $BOOK_DIR/filelist.txt
 rm -rf $PRIMARY_OUTPUT > /dev/null
 rm -rf $SECONDARY_OUTPUT > /dev/null 
@@ -33,6 +36,11 @@ rm -rf $SPELLCHECKED_HOCR_SELECTED > /dev/null
 rm -rf $COMBINED_HOCR > /dev/null
 rm -rf $TEXT_SELECTED > /dev/null
 rm -rf $TESS_OUTPUT > /dev/null
+#rm -rf $JPG_GRAY_IMAGES > /dev/null
+#rm -rf $JPG_COLOR_IMAGES > /dev/null
+rm  -rf $SIDE_BY_SIDE_VIEW > /dev/null
+
+
 mkdir $HOCR_OUTPUT
 mkdir  $PRIMARY_OUTPUT
 mkdir  $SECONDARY_OUTPUT
@@ -41,6 +49,9 @@ mkdir $HOCR_SELECTED
 mkdir $SPELLCHECKED_HOCR_SELECTED
 mkdir $COMBINED_HOCR
 mkdir $TEXT_SELECTED
+mkdir $SIDE_BY_SIDE_VIEW
+#mkdir $JPG_GRAY_IMAGES
+#mkdir $JPG_COLOR_IMAGES
 
 #Job names
 JOB_NAME_BASE=$barebookname-$DATE
@@ -81,7 +92,7 @@ qsub -N $LYNX_DUMP_JOB_NAME -p -150 -hold_jid $OCR_BATCH_JOB_NAME -o $OUTPUT_DIR
 #3. Make a graph of page# vs. score for these highest-scoring pages.
 qsub -N $SUMMARY_SPLIT_JOB_NAME -p -100 -hold_jid  $LYNX_DUMP_JOB_NAME  -b y -o $OUTPUT_DIR -e $ERROR_DIR -S /bin/bash -V /usr/bin/python $RIGAUDON_HOME/Scripts/summary_split.py $CSV_FILE $HOCR_OUTPUT $SECONDARY_OUTPUT $HOCR_SELECTED $TEXT_SELECTED $GRAPH_IMAGE_FILE
 
-qsub -N $SPELLCHECK_JOB_NAME -pe make 20 -hold_jid  $SUMMARY_SPLIT_JOB_NAME -b y -o $OUTPUT_DIR -e $ERROR_DIR -S /bin/bash -V /usr/bin/python $RIGAUDON_HOME/Scripts/read_dict5.py $DICTIONARY_FILE $TEXT_SELECTED/output*  $SPELLCHECK_FILE
+qsub -N $SPELLCHECK_JOB_NAME -pe make 14 -hold_jid  $SUMMARY_SPLIT_JOB_NAME -b y -o $OUTPUT_DIR -e $ERROR_DIR -S /bin/bash -V /usr/bin/python $RIGAUDON_HOME/Scripts/read_dict5.py $DICTIONARY_FILE $TEXT_SELECTED/output*  $SPELLCHECK_FILE
 
 qsub -N $SPELLREPLACE_JOB_NAME -hold_jid  $SPELLCHECK_JOB_NAME -b y -o $OUTPUT_DIR -e $ERROR_DIR -S /bin/bash -V /usr/bin/python $RIGAUDON_HOME/Scripts/spellcheck_hocr.py $SPELLCHECK_FILE $HOCR_SELECTED   $SPELLCHECKED_HOCR_SELECTED
 
