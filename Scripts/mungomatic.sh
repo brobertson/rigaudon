77d26d6231fb4b08c998a76b8ac7025964bfb79a
Bruce Robertson <brobertson@mta.ca>
2013-03-01 09:08:05 -0500

    simplify output name

--- Scripts/mungomatic.sh
+++ Scripts/mungomatic.sh
@@ -3,13 +3,13 @@ LATIN_HOCR_DIR=$1
 GREEK_HOCR_DIR=$2
 OUTPUT_DIR=$3
 for file in `ls $LATIN_HOCR_DIR/`
- do echo "${file%.*}"
-GREEK_FILE=$GREEK_HOCR_DIR/output-${file%.*}*
+ do echo "$file"
+GREEK_FILE=$GREEK_HOCR_DIR/$file
 
 if [ -e $GREEK_FILE ] 
 then
-~/rigaudon/Scripts/munge_hocr.py   $LATIN_HOCR_DIR/${file%.*}.html $GREEK_HOCR_DIR/output-${file%.*}* /tmp/$file > /tmp/debug
-xsltproc ~/rigaudon/Scripts/add_hocr_css.xsl /tmp/$file > $OUTPUT_DIR/${file%.*}.html
+~/rigaudon/Scripts/munge_hocr.py   $LATIN_HOCR_DIR/$file $GREEK_HOCR_DIR/$file /tmp/$file > /dev/null
+xsltproc ~/rigaudon/Scripts/add_hocr_css.xsl /tmp/$file > $OUTPUT_DIR/$file
  rm /tmp/$file
 fi
 done
