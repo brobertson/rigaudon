e9cfdffabadc93dd172cbd6c7fe4f57d4b03ccd2
Bruce Robertson <brobertson@mta.ca>
2013-04-04 13:43:52 -0300

    create a 3d graph of results

--- Scripts/summary_split.py
+++ Scripts/summary_split.py
@@ -1,18 +1,46 @@
 #!/usr/bin/python
 import matplotlib
 matplotlib.use('Agg')
+##from mpl_toolkits.mplot3d import Axes3D
+##from matplotlib.collections import PolyCollection
+##import matplotlib.pyplot as plt
+##import sys
+##import os.path
+##import shutil
+##from operator import itemgetter
+##import numpy as np
+##from matplotlib.colors import colorConverter
+##from pylab import *
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.collections import PolyCollection
+from matplotlib.colors import colorConverter
+import matplotlib.pyplot as plt
+import numpy as np
 import sys
 import os.path
+import os
 import shutil
 from operator import itemgetter
-import numpy as np
-from pylab import *
+
+#import pylab as plab
+
+#from pylab import plot 
 summary_file = sys.argv[1]
 hocr_in_directory=sys.argv[2]
 txt_in_directory=sys.argv[3]
 hocr_out_directory=sys.argv[4]
 txt_out_directory=sys.argv[5]
 graph_image_file=sys.argv[6]
+try:
+        volume_id = sys.argv[7]
+except:
+        volume_id = ""
+try:
+        classifier_name = sys.argv[8]
+except:
+        classifier_name = ""
+
 summary = open(summary_file)
 
 dictionary = {}
@@ -34,22 +62,52 @@ for line in summary:
 
 #This makes a 3-d graph of all the results
 #needs matplotlib version => 1.0
-#fig = plt.figure()
-#ax = fig.gca(projection='3d')
-#verts = []
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+cc = lambda arg: colorConverter.to_rgba(arg, alpha=0.1)
+verts = []
 x = 0
 threed=""
+max_b_score = 0
+max_threshold = 0
+min_threshold = 255
 for key in sorted(dictionary.keys()):
+	this_image = []
 	values = dictionary[key]
 	values = sorted(values, key=itemgetter(0))
 	x = x + 1
 	for pair in values: 
 		threed += str(x) + " " + str(key) + " " + str(pair[0]) + " " + str(pair[1]) + "\n"
-#		verts.append([pair[0],pair[1]])
-#poly = PolyCollection(verts)
-#poly.set_alpha(0.7)
-#ax.add_collection3d(poly, zs = range[0:x], zdir = 'z')
-#plt.show()
+		this_image.append((pair[0],pair[1]))
+		if pair[1] > max_b_score:
+                        max_b_score = pair[1]
+                if pair[0] > max_threshold:
+                        max_threshold = pair[0]
+                if pair[0] < min_threshold:
+                        min_threshold = pair[0]
+	this_image.insert(0,(this_image[0][0] -1,0))
+	this_image.append((this_image[-1][0] + 1, 0))
+	verts.append(this_image)
+
+blueish = colorConverter.to_rgba('b', alpha=0.5)
+#print verts
+poly = PolyCollection(verts,closed=False,edgecolors=[blueish], linewidths=[1], facecolors = [cc('y')])
+
+#poly.set_alpha(0.1)
+ax.add_collection3d(poly, zs = range(0,x), zdir = 'y')
+
+ax.set_xlabel('Binarization threshold')
+ax.set_xlim3d(min_threshold, max_threshold)
+ax.set_ylabel('Page number')
+ax.set_ylim3d(-1, len(verts))
+ax.set_zlabel('B-score')
+
+graph_title = volume_id + " " + classifier_name + '\n'+os.getenv('GAMERA_CMDS', '[no gamera cmds]')
+ax.set_title(graph_title, fontsize="xx-small")
+ax.set_zlim3d(0, max_b_score)
+fig.savefig(os.path.join(hocr_out_directory, "3d.png"))
+
+#now make 3d scores file
 td_scores_file = open(os.path.join(hocr_out_directory, "3d_scores.txt"),'w+')
 td_scores_file.write(threed)
 # now pass threed through gnuplot
@@ -84,8 +142,13 @@ best_scores_file.write(out)
 best_scores = ""
 #now copy them from the source file to the target directory
 #now pass best_scores through gnuplot
-page_bscores_np = np.array(page_bscores_sorted)
-plot(page_bscores_np[:,0],page_bscores_np[:,1])
-savefig(graph_image_file)
+#needs matplotlib version => 1.0
 
-	
+
+fig2 = plt.figure()
+page_bscores_np = np.array(page_bscores_sorted)
+plt.ylim(0,0.75)
+plt.plot(page_bscores_np[:,0],page_bscores_np[:,1])
+plt.title(graph_title, fontsize="xx-small")
+
+plt.savefig(graph_image_file)
