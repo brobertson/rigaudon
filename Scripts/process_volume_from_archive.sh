dc98f81be79ddff989c76f3f6b66d88fe34a3705
Bruce Robertson <brobertson@mta.ca>
2013-04-26 07:58:40 -0400

    broader binarization range

--- Scripts/process_volume_from_archive.sh
+++ Scripts/process_volume_from_archive.sh
@@ -2,8 +2,7 @@
 export HOCR="ABBYY" 
 #export RIGAUDON_HOME=/home/broberts/rigaudon/
 #export FBEVALUATOR_HOME=/home/broberts/Federicos-evaluator/
-export GAMERA_CMDS=" --split --autogroup  --filter --otsu  0.92,0.95,0.97,1.0,1.03,1.05,1.07,1.09,1.12,1.15,1.17,1.19"
-#0.91,0.95,1.0,1.05,1.08" 
+export GAMERA_CMDS=" --split --autogroup  --filter --otsu  0.74,0.76,0.78,0.80,0.82,0.84,0.86,0.88,0.90,0.92,0.94,0.95,0.97,1.0,1.03,1.05,1.07,1.09,1.12,1.15,1.17,1.18,1.19,1.20,1.21,1.22,1.23,1.24,1.25,1.26,1.27"
 export ARCHIVE_ID=$1
 export CLASSIFIER_DIR=$2
 export TEXT_STAGING_DIR=/usr/local/OCR_Processing/Texts/
