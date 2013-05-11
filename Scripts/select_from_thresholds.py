844ae7f2d8d5601e4913b310009c034afadb7a74
Bruce Robertson <brobertson@mta.ca>
2013-05-11 07:01:18 -0300

    new, faster select from thresholds

--- Scripts/select_from_thresholds.py
+++ Scripts/select_from_thresholds.py
@@ -5,6 +5,7 @@ init_gamera()
 import lxml
 from lxml import etree
 import sys
+from operator import itemgetter, attrgetter
 DEBUG=False
 class hocrWord():
     """associates word text with bbox"""
@@ -38,6 +39,7 @@ def get_hocr_lines_for_tree(treeIn):
     hocr_line_elements = treeIn.xpath("//html:span[@class='ocr_line'] | //span[@class='ocr_line']",namespaces={'html':"http://www.w3.org/1999/xhtml"})
     line_counter = 0
     lines_out = []
+    all_words = []
     for hocr_line_element in hocr_line_elements:
         #print "line: ", line_counter, parse_bbox(hocr_line_element.get('title'))
         line_counter += 1
@@ -61,11 +63,12 @@ def get_hocr_lines_for_tree(treeIn):
             aWord.element = word
             words_out.append(aWord)
         aLine = hocrLine()
+        all_words = all_words + words_out
         aLine.words = words_out
         aLine.element = hocr_line_element
         aLine.bbox = parse_bbox(hocr_line_element.get('title'))
         lines_out.append(aLine)
-    return lines_out
+    return lines_out, all_words
 
 def dump_lines(lines_out):
     for line_out in lines_out:
@@ -211,7 +214,42 @@ def memoized_in_dict_lower(memo, dictionary, word):
         return result
 
 
+def close_enough(bbox1, bbox2):
+    fudge = 2
+    total_diff = (abs(bbox1.lr_x - bbox2.lr_x) + abs(bbox1.lr_y - bbox2.lr_y) + abs(bbox1.ul_x  - bbox2.ul_x) + abs(bbox1.ul_y - bbox2.ul_y))
+    if total_diff < fudge * 4:
+        return True
+    else:
+        return False
+
+
+def sort_words_bbox(words):
+    words.sort( key=attrgetter('bbox.lr_y'))
+    words.sort( key=attrgetter('bbox.lr_x'))
+    words.sort(key=attrgetter('text'))
+    return words
+
+
+def score_word(word):
+    IN_DICT_SCORE = 1000
+    IN_DICT_LOWER_SCORE = 100
+    CAMEL_CASE_SCORE = 1
+    ALL_CAPS_SCORE = 10
+    score_total = 0
+    if in_dict(dictionary, word):
+        score_total = score_total + IN_DICT_SCORE
+    elif in_dict_lower(dictionary, word):
+            score_total = score_total + IN_DICT_LOWER_SCORE
+    if score_total > 0:
+        if word.istitle():
+            score_total = score_total + CAMEL_CASE_SCORE
+        elif word.isupper():
+            score_total = score_total + ALL_CAPS_SCORE
+    return score_total
+
+
 def select_best(dictionary,right_lines):
+
     import unicodedata
     if DEBUG: print 'linematches length: ', len(right_lines)
     from greek_tools import in_dict, in_dict_lower, is_greek_string, is_number, split_text_token
@@ -257,19 +295,91 @@ dictionaryFile = codecs.open(sys.argv[1],'r',encoding='UTF-8')
 for line in dictionaryFile:
     (word,freq) = line.split(',')
     dictionary.append(word)
+dictionary = set(dictionary)
 fileIn1 = open(sys.argv[2],'r')
 tree1 = etree.parse(fileIn1)
+lines_1, words_1 = get_hocr_lines_for_tree(tree1)
+sort_words_bbox(words_1)
+print "words_1:"
+for word in words_1:
+    print word.text, word.bbox
+other_words = []
 for fileInName in sys.argv[3:-2]:
     try:
         print "processing:", fileInName
         fileIn2 = open(fileInName,'r')
         tree2 = etree.parse(fileIn2)
-        lines_1 = get_hocr_lines_for_tree(tree1)
-        lines_2 = get_hocr_lines_for_tree(tree2)
-        my_line_matches = compare_hocr_lines(lines_1, lines_2,15,15)
-        select_best(dictionary,my_line_matches)
+        lines_2, words_2 = get_hocr_lines_for_tree(tree2)
+        print
+        print "next word:"
+        for word in words_2:
+            print word.text, word.bbox
+        other_words = other_words + words_2
     except Exception as e:
-        print e 
+        print e
+
+sort_words_bbox(other_words)
+positional_lists = []
+positional_list = []
+x = 0
+while x < len(other_words):
+    print other_words[x].text, other_words[x].bbox
+    try:
+        if len(positional_list) == 0:
+            positional_list.append(other_words[x])
+        else:
+            if close_enough(other_words[x -1].bbox,other_words[x].bbox):
+                print "same as previous"
+                #skip if the text is the same, so that we just get unique texts for this position
+                if not other_words[x-1].text == other_words[x].text:
+                    print "to be added"
+                    positional_list.append(other_words[x])
+            else:
+                if not x == 0:
+                    positional_lists.append(positional_list)
+                    positional_list = []
+    except IndexError:
+        pass
+    x = x + 1
+# we now have a list of list of unique words for each position
+# let's select from each the first one that passes spellcheck
+from greek_tools import in_dict, in_dict_lower, is_greek_string, is_number, split_text_token
+in_dict_memo = {}
+in_dict_memo_lower = {}
+replacement_words = []
+
+#make a 'replacement_words' list with all of the best, non-zero-scoring
+#suggestions for each place
+for positional_list in positional_lists:
+    for word in positional_list:
+        word.score = score_word(word.text)
+    positional_list.sort(key=attrgetter('score'), reverse=True)
+    if positional_list[0].score > 0:
+        replacement_words.append(positional_list[0])
+
+#use a scoring system for the words, e.g. CamelCase, sort of dict it's in, etc.
+#now replace the originals
+for word in words_1:
+    for replacement_word in replacement_words:
+        word.score = score_word(word.text)
+        if close_enough(word.bbox,replacement_word.bbox) and (word.score < replacement_word.score):
+            print "replacing", word.text, word.score, "with", replacement_word.text, replacement_word.score
+            word.element.text = replacement_word.text
+
+
+if DEBUG:
+    print "replacment words:"
+    for word in replacement_words:
+        print word.text
+
+
+    for positional_list in positional_lists:
+        print "##"
+        for word in positional_list:
+            print word.bbox, word.text
+
+
+
 out_file = open(sys.argv[-1],'w')
 out_file.write(etree.tostring(tree1.getroot(), xml_declaration=True))
 out_file.close()
