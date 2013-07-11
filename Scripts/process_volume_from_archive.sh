203a4b1ae858cd1495bfe112d63419941929648e
fbaumgardt <fbaumgardt@e-humanities.net>
2013-07-11 15:36:06 +0200

    Switch between jp2 / tif with wget instead of curl.

--- Scripts/process_volume_from_archive.sh
+++ Scripts/process_volume_from_archive.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 export HOCR="ABBYY" 
-#export RIGAUDON_HOME=/home/broberts/rigaudon/
-#export FBEVALUATOR_HOME=/home/broberts/Federicos-evaluator/
+export RIGAUDON_HOME=/home/broberts/rigaudon/
+export FBEVALUATOR_HOME=/home/broberts/Federicos-evaluator/
 GAMERA_CMDS_DEFAULT=" --split --autogroup  --filter --otsu  0.69,0.72,0.75,0.78,0.81,0.84,0.87,0.91,0.94,0.97,1.0,1.03,1.05,1.07,1.09,1.12,1.15,1.17,1.19,1.22"
 ${GAMERA_CMDS:=$GAMERA_CMDS_DEFAULT}
 export ARCHIVE_ID=$1
@@ -9,7 +10,6 @@ export CLASSIFIER_DIR=$2
 export TEXT_STAGING_DIR=/usr/local/OCR_Processing/Texts/
 
 export PROCESSING_DIR=$TEXT_STAGING_DIR/$ARCHIVE_ID
-
 
 if [ -e $TEXT_STAGING_DIR/AllGreekFromArchiveCombined/${ARCHIVE_ID}_hocr ]; then
   mkdir $PROCESSING_DIR
@@ -23,11 +23,11 @@ if [ ! -d $PROCESSING_DIR ]; then
   mkdir $PROCESSING_DIR
   cd $PROCESSING_DIR
   echo "Attempting to download $ARCHIVE_ID from archive.org"
-  curl -IL "http://www.archive.org/download/${ARCHIVE_ID}/${ARCHIVE_ID}_jp2.zip" >  /tmp/response.txt 
-  grep HTTP/1.1 /tmp/response.txt | tail -1 | grep 404
+  wget --spider "http://www.archive.org/download/${ARCHIVE_ID}/${ARCHIVE_ID}_jp2.zip" 2>  /tmp/response.txt 
+  grep '200 OK' /tmp/response.txt | wc -l
   badDL=$?
   echo "status: $badDL"
-  if [ "$badDL" -eq "0" ]; then
+  if [ "$badDL" == "0" ]; then
     #the jp2 archive is not available, so we'll guess that it's tiff
     wget http://www.archive.org/download/${ARCHIVE_ID}/${ARCHIVE_ID}_tif.zip
   else
@@ -45,12 +45,6 @@ if [ ! -d $PROCESSING_DIR ]; then
   #echo "hocrDir: $hocrDir"
   mkdir $HOCR_DIR
   python $RIGAUDON_HOME/Scripts/abbyy2hocr_etree.py ${ARCHIVE_ID}_abbyy $HOCR_DIR  ${ARCHIVE_ID}_jp2
-  #echo "Flattening abbyy file"
-  #$RIGAUDON_HOME/Scripts/flatten_for_abbyy.sh ${ARCHIVE_ID}_abbyy 
-  #echo 'Converting abbyy.xml file to hocr'
-  #$RIGAUDON_HOME/Scripts/abbyy_to_hocr.sh ${ARCHIVE_ID}_abbyy_flat.xml
-  #echo 'Renumbering HOCR files to concur with image files'
-  #$RIGAUDON_HOME/Scripts/renumber_hocr_out.sh ${ARCHIVE_ID}_jp2
 fi
 #done downloading and pre-processing
 
