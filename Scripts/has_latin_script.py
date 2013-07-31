60bed7db2556f3426c4ae00b6dc98c380f724b9e
Bruce Robertson <brobertson@mta.ca>
2013-07-31 15:21:34 -0400

    fix HOCR mixing

--- Scripts/has_latin_script.py
+++ Scripts/has_latin_script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # vim: set fileencoding=UTF-8 :
-def is_greek_text(url):
+def is_latin_text(url):
 	import urllib2
 	import sys
 	f = urllib2.urlopen(url)
@@ -9,6 +9,7 @@ def is_greek_text(url):
         		return True
 	return False
 import sys
-print is_greek_text("http://www.archive.org/download/" + sys.argv[1] +  "/" + sys.argv[1] + '_djvu.txt')
+print 'testing', sys.argv[1], ' Is latin text?'
+print is_latin_text("http://www.archive.org/download/" + sys.argv[1] +  "/" + sys.argv[1] + '_djvu.txt')
         
 
